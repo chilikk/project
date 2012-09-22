39f047482c38b6d5def1d429be701303e8947386
Denys Knertser <denys@knertser>
2012-09-22 23:22:49 +0200

    class Polling

--- task2.py
+++ task2.py
@@ -9,12 +9,49 @@ if __debug__:
 def poll(router):
 	return router.pollLinksLoad()
 
-if __name__=='__main__':
+class Polling(object):
+	def __init__(routers):
+		self.routers = routers
+		self.pool = Pool(processes = len(routers))
+		self.net_states = []
+		self.prevtime, self.prevload = (None, None)
+		self.notfirst = False
+	
+	def poll(router):
+		return router.pollLinksLoad()
+
+	def polling():
+		pollresult = self.pool.map(self.poll, self.routers)
+		avgtime, totload = self.calc_totload(pollresult)
+		if self.notfirst:
+			difftime, bandwidth = calc_bandwidth(avgtime, totload)
+		else:
+			self.notfirst = True
+			difftime, bandwidth = (None, None)
+		self.prevtime, self.prevload = (avgtime, totload)
+		return (difftime, bandwidth)
+		
+
+	def calc_totload(pollresult):
+		avgtime, totload = (.0,0)
+                for polltime, loads in pollresult:
+                        avgtime+=polltime
+                        for load in loads:
+                                totload+=int(load)
+                avgtime/=len(pollresult)
+		return (avgtime, totload)
+
+	def calc_bandwidth(avgtime, totload):
+		difftime = avgtime-self.prevtime
+                bandwidth = int((totload-self.prevload)/difftime)
+                return (difftime,bandwidth)
+
+if __name__=='ololo':
 	pool = Pool(processes = len(routers))
 	net_states = []
 	prevtime, prevload = (None, None)
 	for i in range(20):
-		nexttime = time.time()+10
+		nexttime = time.time()+20
 		pollresult = pool.map(poll,routers)
 		avgtime, totload = (.0,0)
 		for polltime, loads in pollresult:
@@ -28,12 +65,26 @@ if __name__=='__main__':
 			net_states.append((difftime,bandwidth))
 			print "+%f\t\t%d" % (difftime, bandwidth)
 		else:
-			sys.stderr.write("start\ntime difference\ttotal network bandwidth\n")
+			sys.stderr.write("start\ntime difference\t\ttotal network bandwidth\n")
 		prevtime, prevload = (avgtime, totload)
 		try:
-			sleeptime = nexttime-time.time()
-			print sleeptime
-			time.sleep(sleeptime)
+			time.sleep(nexttime-time.time())
 		except Exception:
-			pass
+			sys.exc_clear()
 	print net_states
+
+if __name__=='__main__':
+	polling = Polling(routers)
+	for i in range(5):
+		nexttime = time.time()+20
+		network_state = polling.polling()
+		if network_state[0]:
+			print "+%f\t\t%d" % network_state
+		else:
+			sys.stderr.write("start polling\ntime difference\t\ttotal network bandwidth\n")
+		try:
+			time.sleep(nexttime-time.time())
+		except Exception:
+			sys.exc_clear()
+	print polling.net_states
+			
