629eea6039ddacdbe2c054d562ad32fea676a218
Denys Knertser <denys@knertser>
2012-09-22 19:08:41 +0200

    trying to poll several times

--- task2.py
+++ task2.py
@@ -2,15 +2,21 @@
 
 from task1mp import routers
 from multiprocessing import Pool
+import time
 if __debug__:
 	import sys
 	from task1mp import starttime
 
 def poll(router):
-	time, load = router.pollLinksLoad()
-	sys.stderr.write("Polled %s (links load %s): %f\n" % (router.host,load,time-starttime))
+	polltime, load = router.pollLinksLoad()
+	sys.stderr.write("Polled %s (links load %s): %f\n" % (router.host,load,polltime-starttime))
 	return router
 
 if __name__=='__main__':
 	pool = Pool(processes = len(routers))
-	routers = pool.map(poll,routers)
+	for i in range(5):
+		routers = pool.map(poll,routers)
+		time.sleep(10)
+	for router in routers:
+		print router.linksLoad
+		print
