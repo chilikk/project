bef28db0b517c5b36d027d7a3cb755694f387c4f
Denys Knertser <denys@knertser>
2012-09-23 12:29:51 +0200

    back to Pool

--- task1.py
+++ task1.py
@@ -2,11 +2,13 @@
 
 from my.topology import Topology
 from my.debug import debugmsg
-from multiprocessing import Process
+from multiprocessing import Pool#Process
 
-def getRouterInfo(router):
+def getRouterInfo(routerid):
+	router = routers[routerid]
 	router.getInfo()
-
+	return router
+	
 def getTopology(initial_router):
 	topology = Topology(initial_router)
 	debugmsg('Started')
@@ -16,9 +18,12 @@ def getTopology(initial_router):
 
 if __name__=='__main__':
 	routers = getTopology('192.168.1.10')
-	workers = [ Process(target=getRouterInfo, args=(router,)) for router in routers ]
-	[ worker.start() for worker in workers ]
-	[ worker.join() for worker in workers ]
+	#workers = [ Process(target=getRouterInfo, args=(router,)) for router in routers ]
+	#[ worker.start() for worker in workers ]
+	#[ worker.join() for worker in workers ]
+	nrouters = len(routers)
+	pool = Pool(processes = nrouters)
+	routers = pool.map(getRouterInfo,range(nrouters))
 	debugmsg('Routers info collected')
 	for router in routers:
 		print router
