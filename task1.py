22a605814429b3069bac800db7e5af7abc799852
Denys Knertser <denys@knertser>
2012-09-23 12:11:41 +0200

    different multiprocessing approach: Process

--- task1.py
+++ task1.py
@@ -2,20 +2,23 @@
 
 from my.topology import Topology
 from my.debug import debugmsg
-from multiprocessing import Pool
+from multiprocessing import Process
 
 def getRouterInfo(router):
 	router.getInfo()
-	return router
 
-topology = Topology('192.168.1.10')
-debugmsg('Started')
-topology.get()
-routers = topology.routers
-debugmsg('Topology identified')
+def getTopology(initial_router):
+	topology = Topology(initial_router)
+	debugmsg('Started')
+	topology.get()
+	debugmsg('Topology identified')
+	return topology.routers
+
 if __name__=='__main__':
-	pool = Pool(processes = len(routers))
-	routers = pool.map(getRouterInfo,routers)
+	routers = getTopology('192.168.1.10')
+	workers = [ Process(target=getRouterInfo, args=(router,)) for router in routers ]
+	[ worker.start() for worker in workers ]
+	[ worker.join() for worker in workers ]
 	debugmsg('Routers info collected')
 	for router in routers:
 		print router
