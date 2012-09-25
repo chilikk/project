a7434165d768d07b35f56caf46fdf16cac13f9b0
Denys Knertser <denys@knertser>
2012-09-25 19:35:24 +0200

    formatting output

--- task2.py
+++ task2.py
@@ -22,11 +22,11 @@ if __name__=='__main__':
 		netstate, threshold, alarm = stats.getNetState()
 		if netstate != "start":
 			if threshold:
-				printmsg("\t%7d\t|  %7d  |  %s" % (netstate, threshold, alarm))
+				printmsg("%7d\t|  %7d  |  %s" % (netstate, threshold, alarm))
 			else:
 				printerrmsg("\t%7d" % netstate)
 		else:
-			printerrmsg("start polling\ntime\t\tnetwork load\talarm threshold")
+			printerrmsg("start polling\n--------------------------\ntime\t\tnetwork load\t\talarm threshold")
 		try:
 			time.sleep(nexttime-time.time())
 		except Exception:
